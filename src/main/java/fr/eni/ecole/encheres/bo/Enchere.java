a6dda2a22597ab27ab859ada8a383201f02d24ab
Annest WHELDON <annest.wheldon2021@campus-eni.fr>
2022-01-12 14:37:50 +0100

    Bo Enchere

--- src/main/java/fr/eni/ecole/encheres/bo/Enchere.java
+++ src/main/java/fr/eni/ecole/encheres/bo/Enchere.java
@@ -3,54 +3,72 @@ package fr.eni.ecole.encheres.bo;
 import java.time.LocalDate;
 
 public class Enchere {
+	private ArticleVendu articleAVendre;
 	private LocalDate dateEnchere;
-	private Double montantEnchere;
-	//TOTO ajouter ArticleVendu
-	
-	
+	private Integer montantEnchere;
+	private Utilisateur encherisseur;
+
 	public Enchere() {
-		
+
 	}
-	
-	
-	
-	public Enchere(LocalDate dateEnchere, Double montantEnchere) {
+
+	public Enchere(ArticleVendu articleAVendre, LocalDate dateEnchere, Integer montantEnchere,
+			Utilisateur encherisseur) {
+		this.articleAVendre = articleAVendre;
 		this.dateEnchere = dateEnchere;
 		this.montantEnchere = montantEnchere;
+		this.encherisseur = encherisseur;
 	}
 
-
-
-
-
+	
 
 	public LocalDate getDateEnchere() {
 		return dateEnchere;
 	}
-	public Double getMontantEnchere() {
+
+	public Integer getMontantEnchere() {
 		return montantEnchere;
 	}
+
 	public void setDateEnchere(LocalDate dateEnchere) {
 		this.dateEnchere = dateEnchere;
 	}
-	public void setMontantEnchere(Double montantEnchere) {
+
+	public void setMontantEnchere(Integer montantEnchere) {
 		this.montantEnchere = montantEnchere;
 	}
 
+	public ArticleVendu getArticleAVendre() {
+		return articleAVendre;
+	}
 
+	public void setArticleAVendre(ArticleVendu articleAVendre) {
+		this.articleAVendre = articleAVendre;
+	}
+	
+	
+
+	public Utilisateur getEncherisseur() {
+		return encherisseur;
+	}
+
+	public void setEncherisseur(Utilisateur encherisseur) {
+		this.encherisseur = encherisseur;
+	}
 
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Enchere [dateEnchere=");
+		builder.append("Enchere [articleAVendre=");
+		builder.append(articleAVendre);
+		builder.append(", dateEnchere=");
 		builder.append(dateEnchere);
 		builder.append(", montantEnchere=");
 		builder.append(montantEnchere);
+		builder.append(", utilisateur=");
+		builder.append(encherisseur);
 		builder.append("]");
 		return builder.toString();
 	}
-	
-	
-	
 
 }
