0efd055597b5752f498d4387f879996eca2efc21
Thanh Binh Chu <thanhbinh.chu87@gmail.com>
2022-01-17 13:43:17 +0100

    binh add bll

--- src/main/java/fr/eni/ecole/encheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/ecole/encheres/bll/UtilisateurManager.java
@@ -1,8 +1,20 @@
 package fr.eni.ecole.encheres.bll;
 
+import fr.eni.ecole.encheres.bo.Utilisateur;
+import fr.eni.ecole.encheres.dal.DALException;
+
 public interface UtilisateurManager {
 
 	
+	public void chercherUtilisateur(String login, String motDePasse);
+	
+		
+	
+	public void selectByPseudo(String pseudo) throws DALException, BLLException;
+	
+	public void updatePassword(String password, String pseudo) throws DALException, BLLException ;
+	
+	public void insert(Utilisateur nouvelUtilisateur) throws DALException, BLLException;
 	
 	
 }
